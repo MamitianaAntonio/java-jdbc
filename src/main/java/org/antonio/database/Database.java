5fb2feadab9d472baf9d858570bd630328e6c5ad
Mamitiana Antonio <ratolojanaharyantonio1@gmail.com>
2025-12-01 14:59:30 +0300

    feat : implements database class for connection

---
+++ src/main/java/org/antonio/database/Database.java
@@ -0,0 +1,15 @@
+package org.antonio.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+  private static final String URL = "jdbc:postgresql://localhost:5432/java_jdbc";
+  private static final String USER = "postgres";
+  private static final String PASSWORD = "Antonio@01";
+
+  public Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(URL, USER, PASSWORD);
+  }
+}
