91708ce35e49b3b413f4e3629a5f5d2df6e455a1
Mamitiana Antonio <ratolojanaharyantonio1@gmail.com>
2025-12-01 15:59:43 +0300

    implement short test on App class and add toString in User

--- src/main/java/org/antonio/App.java
+++ src/main/java/org/antonio/App.java
@@ -1,4 +1,23 @@
 package org.antonio;
 
+import org.antonio.dao.UserDAO;
+import org.antonio.model.User;
+
+import java.util.List;
+
 public class App {
+  public static void main(String[] args) {
+    UserDAO userDAO = new UserDAO();
+
+    try {
+      userDAO.addUser(new User(1, "RATOLOJANAHARY", "ratolojanaharyantonio.com"));
+      List<User> users = userDAO.getAllUsers();
+
+      for (User user : users) {
+        System.out.println(user);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
